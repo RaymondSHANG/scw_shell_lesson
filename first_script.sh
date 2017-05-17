b50ec0c209060099be128e058993b4e00cbf82fb
Raymond2014 <shangyuan5000@gmail.com>
2017-05-17 12:21:09 -0700

    testmessage

--- first_script.sh
+++ first_script.sh
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 if [ "$1" == "-h" ] || [ "$1" == "--help" ] ; then
 	echo "Usage: sh $0 input_file output_file"
 	exit
@@ -9,4 +10,9 @@ head -5 "$1" |sort|wc -c >"$2"
 =======
 head -5 $1 |sort|wc -c >$2
 >>>>>>> parent of bc83e83... Add comments to the sript
+=======
+# This is a test bash file.
+#This script is used to count the number of characters in a file
+head -5 $1 |sort|wc -c >$2
+>>>>>>> parent of 7a289de... Add comments and help,more robust
 
